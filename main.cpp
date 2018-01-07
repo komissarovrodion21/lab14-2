19322d2766343f4fb59cdee96805e4e8748eda72
lightman1998 <kall-lightman@yandex.ru>
2018-01-07 05:56:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,11 @@
 #include <boost/program_options.hpp>
 #include <boost/process.hpp>
 
-namespace bp = boost::process; //подпространство bp для системных процессов
-namespace po = boost::program_options; //подпространство po для разбора командной строки
+namespace bp = boost::process;
+namespace po = boost::program_options;
 
 int _time = 0;
 
-//#1
 bp::child makeProject(std::string build = "Debug") {
 	std::string path = "cmake -H. -Bexamples/_build -DCMAKE_INSTALL_PREFIX=_install -DCMAKE_BUILD_TYPE=" + build;
 
@@ -24,7 +23,6 @@ bp::child makeProject(std::string build = "Debug") {
 	return c;
 }
 
-//#2
 bp::child buildProject() {
 	std::string path = "cmake --build examples/_build";
 
@@ -40,7 +38,6 @@ bp::child buildProject() {
 	return c;
 }
 
-//#3
 bp::child setTargets(std::string Target) {
 	std::string path = "cmake --build examples/_build --target " + Target;
 
@@ -51,31 +48,27 @@ bp::child setTargets(std::string Target) {
 
 int main(int argc, char const *argv[]) {
 	try {
-		// Declare the supported options. (Объявить поддерживаемые параметры.)
-		po::options_description desc("Allowed options"); //хранит все разрешенные опции
+		po::options_description desc("Allowed options");
 		desc.add_options()
-			("help", "выводим вспомогательное сообщение") //имя опции, описание параметра для пользователя, 
-			("config", po::value<std::string>(), "указываем конфигурацию сборки (по умолчанию Debug)") //второй аргумент говорит о типе, к которому будет приводится аргумент
+			("help", "выводим вспомогательное сообщение")
+			("config", po::value<std::string>(), "указываем конфигурацию сборки (по умолчанию Debug)")
 			("install", "добавляем этап установки (в директорию _install)")
 			("pack", "добавляем этап упакови (в архив формата tar.gz)")
 			("timeout", po::value<int>(), "указываем время ожидания (в секундах)");
 
-		po::variables_map vm; //variables_map класс для хранения значений опций(для хранения значений параметров) 
-		po::store(po::parse_command_line(argc, argv, desc), vm); //парсинг аргументов и сохранение их в переменную vm
-																 //вызов функции store-добавить значения,которые нашли в командной строке; parse_command_line - функция для парсинга компонентов; 
+		po::variables_map vm; 
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+																 
+		po::notify(vm);
 
-		po::notify(vm); //notify может использоваться для сообщения о любых ошибках при передаче параметров
-
-
-		if (vm.count("help")) // Check if an option was passed in the command line (Передана ли опция в командной строке)
+		if (vm.count("help"))
 		{
 			std::cout << desc << std::endl;
 			return 1;
 		}
-
-		//конфигурация сборки <Release|Debug>
+    
 		else if (vm.count("config")) {
-			std::string conf(vm["config"].as<std::string>()); // Функция.as<Type>(), применённая к этой структуре, возвращает значение аргумента опции, расценивая информацию для значения как объект типа Type
+			std::string conf(vm["config"].as<std::string>());
 
 			if (makeProject(conf).exit_code())
 				throw std::runtime_error("ERROR: Make project fail!");
@@ -83,7 +76,6 @@ int main(int argc, char const *argv[]) {
 			std::cout << "Result:\t" << buildProject().exit_code() << std::endl;
 		}
 
-		//этап установки в директорию _install
 		else if (vm.count("install")) {
 			if (makeProject().exit_code())
 				throw std::runtime_error("ERROR: Make project fail!");
@@ -94,7 +86,6 @@ int main(int argc, char const *argv[]) {
 			std::cout << "Result:\t" << setTargets("install").exit_code() << std::endl;
 		}
 
-		//этап упаковки в архив формата tar.gz
 		else if (vm.count("pack")) {
 			if (makeProject().exit_code())
 				throw std::runtime_error("ERROR: Make project fail!");
@@ -105,7 +96,6 @@ int main(int argc, char const *argv[]) {
 			std::cout << "Result:\t" << setTargets("package").exit_code() << std::endl;
 		}
 
-		//время ожидания
 		else if (vm.count("timeout")) {
 			int time = vm["timeout"].as<int>();
 
